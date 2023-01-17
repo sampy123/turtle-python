d0547ecc0fbb2b9a85e11d92003e66c14900c326
Sampana Abdou Latif <abdoulsa95@gmail.com>
2023-01-17 10:18:18 -0500

    generate a random walk with random rgb colours

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+import turtle as t
 import random
 
-tim = Turtle()
+tim = t.Turtle()
+t.colormode(255)
 
-colours = ["deep sky blue", "navy", "chocolate",
-           "dark olive green", "crimson", "dark magenta", "deep pink"]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
 
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
 for _ in range(200):
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
+
 
 screen = Screen()
 screen.exitonclick()
