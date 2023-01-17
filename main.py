bb1f74d8b203a6879dfa663ff9cefd9370576679
Sampana Abdou Latif <abdoulsa95@gmail.com>
2023-01-17 09:56:41 -0500

    draw different shapes

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+num_sides = 5
 
+colours = ["deep sky blue", "navy", "chocolate",
+           "dark olive green", "crimson", "dark magenta", "deep pink"]
+
+
+def draw_shape(num_sides):
+    for _ in range(num_sides):
+        angle = 360 / num_sides
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
