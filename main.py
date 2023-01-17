8dd78567c0c0bc05c0c2dbfaa15fbaf9166a5471
Sampana Abdou Latif <abdoulsa95@gmail.com>
2023-01-17 11:18:03 -0500

    extract rgb values from colours.jpg

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
-from turtle import Screen
-import turtle as t
-import random
+import colorgram
 
-tim = t.Turtle()
-t.colormode(255)
+rgb_colors = []
+colors = colorgram.extract('colours.jpg', 30)
 
-
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    random_color = (r, g, b)
-    return random_color
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
 
 
-tim.speed("fastest")
+print(rgb_colors)
 
-
-def draw_spirograph(size_of_gap):
-    for _ in range(int(360 / size_of_gap)):
-        tim.color(random_color())
-        tim.circle(100)
-        tim.setheading(tim.heading() + size_of_gap)
-
-
-draw_spirograph(3)
-
-
-screen = Screen()
-screen.exitonclick()
+[(220, 218, 211), (194, 176, 154), (224, 233, 225), (20, 43, 73), (205, 193, 161), (26, 115, 160), (141, 70, 58), (29, 127, 78), (16, 43, 29), (135, 159, 179), (206, 215, 223), (210, 136, 15), (219, 129, 161), (64, 39, 26), (204, 85, 125),
+ (231, 216, 221), (19, 60, 117), (158, 77, 109), (118, 27, 21), (61, 41, 52), (145, 158, 150), (106, 122, 162), (131, 18, 22), (204, 186, 182), (204, 65, 60), (225, 169, 186), (56, 159, 101), (84, 61, 40), (34, 84, 46), (171, 206, 179)]
